336d9db0ae6abfafadd5b953f3fbfa23d06963e7
cristinatanase1989 <110465755+cristinatanase1989@users.noreply.github.com>
2022-09-21 15:27:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hola");
+
+node=1;
+console.log(node);
+let as1=1;
+as1 != 1;
+
+for(let i = 0; i<=100; i++){
+    console.log(i)
+}
+
+for(let j = 0; j<=100; j++){
+
+    if(j % 2 ==0){
+        console.log(j)
+    }
+}
+
+while
